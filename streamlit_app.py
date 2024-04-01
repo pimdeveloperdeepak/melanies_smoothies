fea8617bf8aa046a39cd0279e71b59c71e9b66ac
pimdeveloperdeepak <165408830+pimdeveloperdeepak@users.noreply.github.com>
2024-04-01 21:31:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,28 +23,28 @@ ingredients_list = st.multiselect(
     ,max_selections=5
 )
 if ingredients_list:
-     ingredients_string =''
+         ingredients_string =''
  
-     for fruit_chosen in ingredients_list:
-         ingredients_string += fruit_chosen + ' '
-         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-         fv_df = st.dataframe(data =fruityvice_response.json(),use_container_width=True)
+         for fruit_chosen in ingredients_list:
+             ingredients_string += fruit_chosen + ' '
+             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+             fv_df = st.dataframe(data =fruityvice_response.json(),use_container_width=True)
 
-     #st.write(ingredients_string)
 
-     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+
+         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order+ """')"""
      
-     st.write(my_insert_stmt)
-     #st.stop()
+         st.write(my_insert_stmt)
+
     
-     time_to_insert = st.button('Submit Order')
+         time_to_insert = st.button('Submit Order')
     
-     if time_to_insert:
-         session.sql(my_insert_stmt).collect()
-         #st.success('Your Smoothie is ordered!', icon="✅")
-         #st.success(f"Your Smoothie with ingredients '{ingredients_string}' for '{name_on_order}' is ordered!", icon="✅")
-         st.success(f"Your Smoothie is ordered, '{name_on_order}'", icon="✅")
+         if time_to_insert:
+             session.sql(my_insert_stmt).collect()
+             #st.success('Your Smoothie is ordered!', icon="✅")
+             #st.success(f"Your Smoothie with ingredients '{ingredients_string}' for '{name_on_order}' is ordered!", icon="✅")
+             st.success(f"Your Smoothie is ordered, '{name_on_order}'", icon="✅")
 
 
 import requests
