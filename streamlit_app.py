a0b8645ed71357d2e59be8605e0f170166077bba
pimdeveloperdeepak <165408830+pimdeveloperdeepak@users.noreply.github.com>
2024-04-05 19:26:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -23,32 +24,30 @@ ingredients_list = st.multiselect(
     ,max_selections=5
 )
 if ingredients_list:
-     #st.write(ingredients_list)
-     #st.text(ingredients_list) 
-     ingredients_string =''
+         ingredients_string =''
  
-     for fruit_chosen in ingredients_list:
-         ingredients_string += fruit_chosen + ' '
-     #st.write(ingredients_string)
+         for fruit_chosen in ingredients_list:
+                 ingredients_string += fruit_chosen + ' '
+                 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+                 fv_df = st.dataframe(data =fruityvice_response.json(),use_container_width=True)
 
-     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order+ """')"""
+
+
+         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                values ('""" + ingredients_string + """','""" + name_on_order+ """')"""
      
-     st.write(my_insert_stmt)
-     #st.stop()
+         st.write(my_insert_stmt)
+     
     
-     time_to_insert = st.button('Submit Order')
+         time_to_insert = st.button('Submit Order')
     
-     if time_to_insert:
-         session.sql(my_insert_stmt).collect()
-         #st.success('Your Smoothie is ordered!', icon="✅")
-         #st.success(f"Your Smoothie with ingredients '{ingredients_string}' for '{name_on_order}' is ordered!", icon="✅")
-         st.success(f"Your Smoothie is ordered, '{name_on_order}'", icon="✅")
+        if time_to_insert:
+             session.sql(my_insert_stmt).collect()
+             #st.success('Your Smoothie is ordered!', icon="✅")
+             #st.success(f"Your Smoothie with ingredients '{ingredients_string}' for '{name_on_order}' is ordered!", icon="✅")
+             st.success(f"Your Smoothie is ordered, '{name_on_order}'", icon="✅")
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data =fruityvice_response.json(),use_container_width=True)
+
 
     
