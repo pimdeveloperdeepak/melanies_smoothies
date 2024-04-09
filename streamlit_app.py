47a8349498bcb81389b71d5b272c05f0b2b71cec
pimdeveloperdeepak <165408830+pimdeveloperdeepak@users.noreply.github.com>
2024-04-09 23:16:21 +0530

    Update streamlit_app.py
    Remove whitespace issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
@@ -30,23 +30,23 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
- 
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string.strip() + """','""" + name_on_order + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string.strip() + """', '""" + name_on_order + """')"""
 
     st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
-    
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, '{name_on_order}'", icon="✅")
